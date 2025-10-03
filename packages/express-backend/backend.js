2cd393d5351195799ed25b740f51bdfc174b8d26
klsims04 <klsims@calpoly.edu>
2025-10-02 20:59:28 -0700

    Add POST

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -10,6 +10,19 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
@@ -59,6 +72,17 @@ const users = {
 
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.listen(port, () => {
